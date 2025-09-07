509c4f9c1157cf13df2ca24ae8791730c67588c4
PusiaPaker <antek.achelstowski@gmail.com>
2025-09-07 14:46:19 -0400

    initial project start

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
+
+let size = 100;
 
 function draw() {
   background(220);
+  fill(255, 255, 255);
+  rect(width/2 - (size/2), height/2 - (size/2), size, size);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
