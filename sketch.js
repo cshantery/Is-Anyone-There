93cc4ac398c00c25b77eda08ac6e92eff0122137
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-09-09 19:16:34 -0400

    orientation change

--- sketch.js
+++ sketch.js
@@ -1,3 +1,32 @@
+// globals
+
+// N, E, S, W
+const orientations = ['N', 'E', 'S', 'W']
+let curOrientation = 'N';
+
+// functions
+function updateOrientation(direction){
+    /*
+    direction: 'left' or 'right'
+
+    'right' moves orientation clockwise (think of compass)
+    'left' moves counterclockwise
+
+    apparently javascript's % does not work as expected for negative numbers, we can use n % mod => ((n%m)+m)%m
+    */
+
+    ind = orientations.indexOf(curOrientation); 
+    if(direction == 'left'){
+        curOrientation = orientations[(((ind-1)%4)+4)%4]
+    }
+    else if(direction == 'right'){
+        curOrientation = orientations[(((ind+1)%4)+4)%4]
+    }
+
+    console.log(curOrientation);
+}
+
+//
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -7,7 +36,18 @@ let size = 100;
 function draw() {
   background(20);
   fill(255, 255, 255);
-  rect(width/2 - (size / 2), height/2 - (size / 2), size, size);
+
+  textSize(50)
+  text(`Facing ${curOrientation}`, width/2, height/2)
+}
+
+function keyPressed(){
+  if(key == 'ArrowLeft'){
+    updateOrientation('left')
+  }
+  else if(key == 'ArrowRight'){
+    updateOrientation('right')
+    }
 }
 
 function windowResized() {
