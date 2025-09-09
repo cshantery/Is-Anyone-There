2cf7aa47f7bd945827e0d3e0b7290abe0d64c226
CarsonSherrill <72176166+CarsonSherrill@users.noreply.github.com>
2025-09-09 19:46:19 -0400

    Update sketch.js
    Added timer (Adri) and added check if timer is 0 and displays "test" (Carson)

--- sketch.js
+++ sketch.js
@@ -3,11 +3,26 @@ function setup() {
 }
 
 let size = 100;
+let timerVal = 10
+let startTime
 
 function draw() {
   background(20);
   fill(255, 255, 255);
   rect(width/2 - (size / 2), height/2 - (size / 2), size, size);
+  
+  if(frameCount % 60 == 0 && timerVal >= 1){
+    timerVal--; 
+  }
+
+  textSize(16);
+  textAlign(RIGHT, TOP);
+  text(timerVal, width - 10, 10);
+
+  if(timerVal == 0){
+      text("test", width - 10, 30);
+    }
+
 }
 
 function windowResized() {
