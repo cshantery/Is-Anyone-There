2b5aa8c7614551e496a3e034055b8c979030b87f
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-09-19 13:47:58 -0400

    secret number revealing mechanics

--- src/FileCabinetView.js
+++ src/FileCabinetView.js
@@ -1,5 +1,6 @@
 class FileCabinetObject {
-    constructor(x, y, xsize, ysize, onClick = () => {}){
+    constructor(id, x, y, xsize, ysize, onClick = () => {}){
+        this.id = id; // for now just using index
         this.x = x;
         this.y = y;
         this.xsize = xsize;
@@ -38,10 +39,15 @@ class FileCabinetObject {
 }
 
 class FileCabinetView extends View {
-    constructor() {
+    constructor(cabinetCount=4) {
         super(122, 122, 122, "");
 
         this.cabinets = []
+
+        this.numberVisibiliy = 0; //for fadout, initially invisible
+        this.fadoutRate = 0.03 // smaller, slower
+
+        this.secretNumber = "8"; // number to display
 
         let randx;
         let randy;
@@ -49,25 +55,47 @@ class FileCabinetView extends View {
         let cabinetWidth = 100;
         let cabinetHeight = 200;
 
-        for(let i = 0; i < 4; i++){
+        let chosenCabinet = Math.floor(Math.random() * (cabinetCount)); // the one that hides the number
+
+        for(let i = 0; i < cabinetCount; i++){
             randx = Math.random() * (windowWidth - 2*cabinetWidth) + cabinetWidth
             randy = Math.random() * (windowHeight - 2*cabinetHeight) + cabinetHeight
-            console.log(randx)
-            console.log(randy)
-            this.cabinets.push(new FileCabinetObject(randx, randy, cabinetWidth, cabinetHeight))
+            this.cabinets.push(new FileCabinetObject(i, randx, randy, cabinetWidth, cabinetHeight,
+                (obj) => {
+                    if(obj.id == chosenCabinet){
+                        console.log('You have clicked the chosen cabinet, it was id '+obj.id)
+                        console.log(this.showNumber)
+                        this.numberVisibiliy = 1 //make it totally visible
+                    }
+                    else{
+                        console.log('This file cabinet is locked.')
+                    }
+                }
+            ))
         }
     }
 
     draw(){
         super.draw();
         push();
+        
+        // always exponentially decay visibility of number
+        this.numberVisibiliy = this.numberVisibiliy * (1-this.fadoutRate);
 
-        //textSize(20);
-        //text("File Cabinet room" , width/2, height/2 + 40);
+        // set to 0 so it doesn't stay there for too long (while being barely visible, exp decay gets slower as visibility gets closer to 0)
+        if(this.numberVisibiliy < 0.1){
+            this.numberVisibiliy = 0; 
+        }
+
+        fill(0,0,0, 255*this.numberVisibiliy)
+        textSize(250);
+        text(this.secretNumber, windowWidth/2, windowHeight/2);
+        
         pop();
     }
 
     update(dt) {
+        
     }
 
     keyPressed() {
